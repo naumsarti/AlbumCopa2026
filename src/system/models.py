08cee3a1ea2f7602e040a362771f9088ab57c1ba
naumsarti <naumsarti@gmail.com>
2026-06-06 21:21:32 -0300

    atualizaçao models adicionado atributo quantidade

--- src/system/models.py
+++ src/system/models.py
@@ -1,18 +1,21 @@
 class Figurinha:
-    def __init__(self, id_fig, nome, pais):
+    def __init__(self, id_fig, nome, pais, quantidade=1):
         self.id = int(id_fig)
         self.nome = nome.strip()
         self.pais = pais.strip()
+        self.quantidade = quantidade
 
     def to_dict(self):
         return {
             "id": self.id,
             "nome": self.nome,
-            "pais": self.pais
+            "pais": self.pais,
+            "quantidade": self.quantidade
         }
 
     def __str__(self):
-        return f"[#{self.id:03d}] {self.nome} ({self.pais})"
+        prefixo = f"{self.quantidade}x " if self.quantidade > 1 else ""
+        return f"{prefixo}[#{self.id:03d}] {self.nome} ({self.pais})"
 
 class NodoLista:
     def __init__(self, figurinha):
