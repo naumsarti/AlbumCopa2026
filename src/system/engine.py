3fea08f156d75e41aebd443669a1fc5e5e3508dc
naumsarti <naumsarti@gmail.com>
2026-06-08 18:43:22 -0300

    atualizaçao engine funçao carregar catalogo de figurinhas

--- src/system/engine.py
+++ src/system/engine.py
@@ -12,3 +12,17 @@ class SistemaFigurinhas:
         
         self.catalogo_oficial = {} 
         self.carregar_catalogo_csv()
+
+    def carregar_catalogo_csv(self, arquivo="src/data/album_copa2026.csv"):
+        if not os.path.exists(arquivo):
+            return False, f"[AVISO] Arquivo {arquivo} não encontrado. Buscas textuais não funcionarão."
+            
+        with open(arquivo, mode="r", encoding="utf-8") as f:
+            leitor = csv.DictReader(f, delimiter=";")
+            for linha in leitor:
+                try:
+                    id_fig = int(linha["id"])
+                    self.catalogo_oficial[id_fig] = Figurinha(id_fig, linha["nome"], linha["pais"])
+                except ValueError:
+                    continue
+        return True, "Catálogo carregado com sucesso."
